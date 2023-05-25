bd20adf4ee909ee3880430a65a0f82d62c0a4860
esdncalingo <esdncalingo@gmail.com>
2023-05-25 13:38:49 +0800

    added participants controller

--- db/migrate/20230510014657_create_participants.rb
+++ db/migrate/20230510014657_create_participants.rb
@@ -3,7 +3,7 @@ class CreateParticipants < ActiveRecord::Migration[7.0]
     create_table :participants do |t|
       t.references :user, null: false, foreign_key: true
       t.references :event, null: false, foreign_key: true
-      t.string :status
+      t.string :status, default: 'pending'
       t.string :rank
     end
   end
