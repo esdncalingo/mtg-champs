c6b3d2a61aaf58d1741267d7c5aca042f77d2a06
esdncalingo <esdncalingo@gmail.com>
2023-05-26 16:00:56 +0800

    fix participants submitted deck streams

--- db/migrate/20230510014541_create_events.rb
+++ db/migrate/20230510014541_create_events.rb
@@ -5,6 +5,7 @@ class CreateEvents < ActiveRecord::Migration[7.0]
       t.text :description
       t.datetime :schedule
       t.string :game_format
+      t.boolean :finished, default: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
