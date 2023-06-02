fa9bf14a22e2a40111a94d307e2d03f2ec985fa5
esdncalingo <esdncalingo@gmail.com>
2023-06-02 20:05:26 +0800

    fix cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://mtg-champs-8sufyfpfh-esdncalingo.vercel.app'
+    origins 'https://mtg-champs.vercel.app'
 
     resource "*",
       headers: :any,
