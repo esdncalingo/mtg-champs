3a9b4f461d94d0e3061b0caf883c711d4587322e
esdncalingo <esdncalingo@gmail.com>
2023-06-02 19:43:50 +0800

    edit cors for production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173'
+    origins 'https://mtg-champs-8sufyfpfh-esdncalingo.vercel.app'
 
     resource "*",
       headers: :any,
